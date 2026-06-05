1a0fe07eda66e40b7eddb4e104fbc4b65d892444
guscmiranda <gcmiranda2005@gmail.com>
2026-06-05 18:48:20 -0300

    Added a few comments

--- core/GameStates.py
+++ core/GameStates.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
 class GameState(Enum):
+    '''
+    Ajuda a controlar os estados do jogo,
+    cada um possui métodos de desenho e lida diferente com
+    os updates e inputs do jogador
+    '''
     IN_GAME = 1
     GAME_OVER = 2
     GAME_WIN = 3
