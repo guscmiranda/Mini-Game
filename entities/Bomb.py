088272fdf29d4fdd166ac1dabec0bf5c910fd111
guscmiranda <gcmiranda2005@gmail.com>
2026-06-05 18:48:41 -0300

    Added a few comments

--- entities/Bomb.py
+++ entities/Bomb.py
@@ -5,9 +5,16 @@ import copy
 import math
 
 class Bomb(Entity):
+    '''
+    Inimigo do tipo bomba.
+    Após ser criado, surge fora da tela em uma posição aleatória
+    e passa a perseguir continuamente o jogador.
+    Quando sua vida chega a zero, a entidade é marcada para remoção.
+    '''
 
-    def __init__(self, partes, cores, centro=(0,0), partes_criticas=[], rand_init=False):
+    def __init__(self, partes, cores, centro=(0,0), partes_criticas=[]):
 
+        # Cria cópias para evitar que múltiplas bombas compartilhem as mesmas estruturas.
         super().__init__(
             copy.deepcopy(partes),
             copy.deepcopy(cores),
@@ -19,72 +26,63 @@ class Bomb(Entity):
 
         self.velocidade = 230
 
+        # Controle de temporizador para futuras explosões
         self.bomb_timer = 0
         self.bomb_cooldown = 8
 
-        # estados
-        self.entering = True
+        # Escolha de uma posição inicial fora da tela
+        side = random.choice(['top', 'bottom', 'left', 'right'])
 
-        # direção vertical
-        self.vertical_direction = 1
+        if side == 'top':
+            x = random.randint(0, 1280)
+            y = random.randint(-100, -20)
+            x = random.randint(0, 1280)
+            y = random.randint(740, 820)
+        elif side == 'left':
+            x = random.randint(-100, -20)
+            y = random.randint(0, 720)
+        else:  # right
+            x = random.randint(1300, 1380)
+            y = random.randint(0, 720)
 
-        if rand_init:
-            side = random.choice(['top', 'bottom', 'left', 'right'])
-
-            if side == 'top':
-                x = random.randint(0, 1280)
-                y = random.randint(-100, -20)
-                x = random.randint(0, 1280)
-                y = random.randint(740, 820)
-            elif side == 'left':
-                x = random.randint(-100, -20)
-                y = random.randint(0, 720)
-            else:  # right
-                x = random.randint(1300, 1380)
-                y = random.randint(0, 720)
-        else:
-            x, y = 1600, 360
-
+        # Move a entidade para sua posição inicial
         self.mover(x, y)
 
-    def entrada(self, dt):
-
-        alvo_x = 1200
-
-        if self.cx > alvo_x:
-
-            self.mover(
-                -(self.velocidade-40) * dt,
-                0
-            )
-
-        else:
-            self.entering = False
-
     def seguir_player(self, player, dt):
+        '''
+        Calcula o vetor direção até o jogador,
+        normaliza esse vetor e movimenta a entidade
+        mantendo velocidade constante
+        '''
 
+        # Vetor da bomba até o jogador
         dx = player.cx - self.cx
         dy = player.cy - self.cy
 
+        # Distância entre os dois objetos
         distancia = math.hypot(dx, dy)
 
         if distancia == 0:
             return
 
+        # Normalizando
         dx /= distancia
         dy /= distancia
 
-        self.mover(
+        self.mover( # Movimento na direção do jogador
             dx * self.velocidade * dt,
             dy * self.velocidade * dt
         )
 
 
     def update(self, dt, player):
+        '''
+        Verifica se a entidade ainda está viva e
+        executa sua lógica de movimentação.
+        '''
+
+        # Marca a entidade para remoção quando sua vida acaba
         if self.vida <= 0: self.alive = False
 
-        # if self.entering:
-        #     self.entrada(dt)
-        #
-        # else:
+        # Persegue continuamante !!
         self.seguir_player(player, dt)
