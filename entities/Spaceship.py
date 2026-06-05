93d2f21c960d6d928eb08a247bb4ffe312687991
guscmiranda <gcmiranda2005@gmail.com>
2026-06-05 18:47:15 -0300

    Added a few comments

--- entities/Spaceship.py
+++ entities/Spaceship.py
@@ -6,6 +6,11 @@ import pygame
 import math
 
 class Spaceship(Entity):
+    '''
+   A nave controlada pelo jogador, mantém informações de vida, cooldowns
+   e estados necessários para o controle do jogador.
+   '''
+
     def __init__(self, partes, cores,  centro=(0, 0), partes_criticas=[]):
         super().__init__(partes, cores, centro, partes_criticas)
 
@@ -29,14 +34,22 @@ class Spaceship(Entity):
         self.mover(600, 600)
 
     def shoot(self, partes_criticas):
+        '''
+       Cria e retorna um novo projétil.
+
+       O projétil nasce alguns pixels à frente da nave,
+       seguindo sua direção atual.
+       '''
 
         rad = math.radians(self.angulo)
 
+        # Distância entre o centro da nave e o ponto onde o projétil será criado.
         offset = 100
 
         x = self.cx + math.sin(rad) * offset
         y = self.cy - math.cos(rad) * offset
 
+        # Cria os tiros
         tiro = Projectile(
             tiro_partes,
             tiro_cores,
@@ -49,22 +62,33 @@ class Spaceship(Entity):
 
 
     def handle_input(self, dt):
+        '''
+        Processa todas as entradas do teclado.
+
+        Controla: Movimento, Rotação, Transformação de tamanho
+        '''
+
         velocidade = self.velocidade * dt
         keys = pygame.key.get_pressed()
+        # Mover para cima
         if keys[pygame.K_w]:
             self.mover(0, -velocidade)
+        # Mover para esquerda
         if keys[pygame.K_a]:
             self.mover(-velocidade, 0)
+        # Mover para baixo
         if keys[pygame.K_s]:
             self.mover(0, velocidade)
+        # Mover para direita
         if keys[pygame.K_d]:
             self.mover(velocidade, 0)
-
+        # Girar para esquerda
         if keys[pygame.K_j]:
             self.rotacionar(-200*dt)
+        # Girar para direita
         if keys[pygame.K_k]:
             self.rotacionar(200*dt)
-
+        # Diminuir ou aumentar a nave
         if keys[pygame.K_f] and not self.f_pressed:
             if self.isBig and self.time_since_last_mini > self.cd_mini:
                 self.escalar(0.5, 0.5)
@@ -80,10 +104,18 @@ class Spaceship(Entity):
             self.f_pressed = False
 
     def update(self, dt):
+        '''
+       Atualiza o estado da nave a cada frame.
 
+       Responsável por: Verificar se a nave morreu, processar entradas do jogador,
+       atualizar temporizadores internos
+       '''
+
+        # Se acabarem as vidas ele morre
         if self.vidas <= 0: self.alive = False
 
         self.handle_input(dt)
+        # Incrementa os tempos
         self.time_since_last_shot += dt
         self.time_since_last_mini += dt
         self.time_since_last_damage += dt
